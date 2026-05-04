05ff0774022498d5f0d39d4fbb94d5a54b5f4808
ARod03 <ardgz2003@gmail.com>
2026-05-04 15:54:18 -0500

    Need to make the formula for the second part of the exercise.

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,56 @@
 package com.pluralsight;
 
 public class Reservation {
+
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, double price, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        return this.roomType() + this.price() * this.numberOfNights() * this.isWeekend();
+
+    }
+
+
+
 }
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,59 @@
 package com.pluralsight;
 
 public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    private boolean isDirty;
+    private boolean isOccupied;
+
+
+    public Room(int numberOfBeds, double price, boolean isDirty, boolean isOccupied) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isDirty = isDirty;
+        this.isOccupied = isOccupied;
+    }
+
+        public int getNumberOfBeds() {
+            return numberOfBeds;
+        }
+
+        public void setNumberOfBeds(int numberOfBeds) {
+            this.numberOfBeds = numberOfBeds;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public boolean isDirty() {
+            return isDirty;
+        }
+
+        public void setDirty(boolean dirty) {
+            isDirty = dirty;
+        }
+
+        public boolean isOccupied() {
+            return isOccupied;
+        }
+
+        public void setOccupied(boolean occupied) {
+            isOccupied = occupied;
+    }
+
+    public boolean isAvailable() {
+        if(!this.isOccupied() && !this.isDirty()){
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
